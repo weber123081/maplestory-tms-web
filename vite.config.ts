c71de75b36f03c47a77a9c23b0238b778e6fb327
Your Name <you@example.com>
2026-03-18 14:21:20 +0800

    fix: restore local development base path and fix 404

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    base: '/maplestory-tms-web/',
+    base: mode === 'production' ? '/maplestory-tms-web/' : '/',
     server: {
         port: 3003,
         proxy: {
@@ -15,4 +15,4 @@ export default defineConfig({
             },
         },
     },
-})
+}))
